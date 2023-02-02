1fd10b8ebdf0e3c502be4d00b12e89c7a35d77a4
ofgonen_office <ofgonen@gmail.com>
2023-02-02 18:04:05 +0300

    Implement SharedDataLibrary

---
+++ Assets/Script/FFStudio/Data/Shared/SharedDataLibrary.cs
@@ -0,0 +1,40 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace FFStudio
+{
+// [ CreateAssetMenu( fileName = "shared_", menuName = "FF/Data/Shared/Library/" ) ]
+	public class SharedDataLibrary< DataType > : ScriptableObject 
+	{
+#region Fields
+      [ Title( "Setup" ) ]
+        [ SerializeField ] DataType[] data_array;
+#endregion
+
+#region Properties
+        public int LibrarySize => data_array.Length;
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+        public DataType GetData( int index )
+        {
+			return data_array[ index ];
+		}
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+	}
+}
