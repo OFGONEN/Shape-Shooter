0c89069c3d2f7a19a4c1174a2f35b2f7a9c4a957
ofgonen-home <ofgonen@gmail.com>
2023-02-23 13:06:23 +0300

    Implement ShapeData

---
+++ Assets/Script/ShapeData.cs
@@ -0,0 +1,33 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[ CreateAssetMenu( fileName = "shared_shape_data", menuName = "FF/Game/Shape Data" ) ]
+public class ShapeData : ScriptableObject
+{
+#region Fields
+    public float shape_rotation_angle;
+    public float shape_rotation_duration;
+    public Ease shape_rotation_ease;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
