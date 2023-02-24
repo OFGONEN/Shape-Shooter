ebfa7dd6915f01bb3f86cb9d650ccc08fd3e1371
ofgonen-home <ofgonen@gmail.com>
2023-02-24 13:14:30 +0300

    Edge: UpdateVisual

--- Assets/Script/Edge.cs
+++ Assets/Script/Edge.cs
@@ -3,19 +3,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
 
 public class Edge : MonoBehaviour
 {
 #region Fields
+    [ SerializeField ] ColorSetter _colorSetter;
+    
+    EdgeColorData edge_color_data;
 #endregion
 
 #region Properties
+    public int ColorID => edge_color_data.edge_color_ID;
 #endregion
 
 #region Unity API
 #endregion
 
 #region API
+    public void UpdateVisual( EdgeColorData data )
+    {
+		edge_color_data = data;
+
+		_colorSetter.SetColor( data.edge_color );
+	}
 #endregion
 
 #region Implementation
