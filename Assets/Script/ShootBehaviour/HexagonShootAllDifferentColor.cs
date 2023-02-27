dfa1efff8717b520fbc19183511a6465de605bf8
ofgonen-home <ofgonen@gmail.com>
2023-02-27 10:32:37 +0300

    Implement Hexagon Shoot behaviours

---
+++ Assets/Script/ShootBehaviour/HexagonShootAllDifferentColor.cs
@@ -0,0 +1,52 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonShootAllDifferentColor : ShootBehaviour
+{
+#region Fields
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+    public override void Shoot()
+    {
+		EdgeShooter edgeShooter;
+
+		list_edge_shooter.itemDictionary.TryGetValue( 0, out edgeShooter );
+		edgeShooter.Shoot( library_edge_color.GetRandomDistinctEdgeColorData() );
+
+		list_edge_shooter.itemDictionary.TryGetValue( 1, out edgeShooter );
+		edgeShooter.Shoot( library_edge_color.GetRandomDistinctEdgeColorData() );
+
+		list_edge_shooter.itemDictionary.TryGetValue( 2, out edgeShooter );
+		edgeShooter.Shoot( library_edge_color.GetRandomDistinctEdgeColorData() );
+
+		list_edge_shooter.itemDictionary.TryGetValue( 3, out edgeShooter );
+		edgeShooter.Shoot( library_edge_color.GetRandomDistinctEdgeColorData() );
+
+		library_edge_color.RefreshDistinctColorList();
+
+		list_edge_shooter.itemDictionary.TryGetValue( 4, out edgeShooter );
+		edgeShooter.Shoot( library_edge_color.GetRandomDistinctEdgeColorData() );
+
+		list_edge_shooter.itemDictionary.TryGetValue( 5, out edgeShooter );
+		edgeShooter.Shoot( library_edge_color.GetRandomDistinctEdgeColorData() );
+	}
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
