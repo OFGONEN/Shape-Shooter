d6c377dc9f32b141532381447ea381af6fd05398
ofgonen-home <ofgonen@gmail.com>
2023-03-02 22:10:19 +0300

    UI Manager: Implement Tutorial

--- Assets/Script/FFStudio/Extension/Extensions.cs
+++ Assets/Script/FFStudio/Extension/Extensions.cs
@@ -12,6 +12,7 @@ namespace FFStudio
 	public static class Extensions
 	{
 		public static readonly string SAVE_PATH = Application.persistentDataPath + "/Saves/";
+		public static readonly string Key_Tutorial = "tutorial";
 
 		static List< Transform > baseModelBones   = new List< Transform >( 96 );
 		static List< Transform > targetModelBones = new List< Transform >( 96 );
